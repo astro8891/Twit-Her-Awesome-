ec8e07598a1de2261be8868242f47c017c2ca699
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-26 10:06:13 +1300

    Adding init

---
+++ SharpieTwitHer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharpieTwitHer.Startup))]
+namespace SharpieTwitHer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
