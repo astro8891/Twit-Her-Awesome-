e6f96303a1f4e42e2df933a2348b2b2317a2359a
user <user@gmail.com>
2015-03-27 11:12:18 +1300

    Working solution

--- SharpieTwitHer/Models/Tweet.cs
+++ SharpieTwitHer/Models/Tweet.cs
@@ -25,13 +25,4 @@ namespace SharpieTwitHer.Models
         }
     }
 }
-using System.Text;
 
-namespace SharpieTwitHer.Models
-{
-    public class Tweet
-    {
-        public int ID { get; set; }
-        public string Body { get; set; }
-    }
-}
